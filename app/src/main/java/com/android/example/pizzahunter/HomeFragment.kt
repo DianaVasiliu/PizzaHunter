7495fd5b4a6c242387ef0373e37b7f2279ddf3e4
Diana Elena Vasiliu <diana.vasiliu10@gmail.com>
2022-03-08 16:51:25 +0200

    Added presentation video

--- app/src/main/java/com/android/example/pizzahunter/HomeFragment.kt
+++ app/src/main/java/com/android/example/pizzahunter/HomeFragment.kt
@@ -1,5 +1,6 @@
 package com.android.example.pizzahunter
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -17,6 +18,37 @@ class HomeFragment : Fragment() {
     ): View? {
         (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.home_title)
         val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater, R.layout.fragment_home, container, false)
+
+        binding.videoView.apply {
+            setVideoPath(
+                "android.resource://" +
+                        (activity as AppCompatActivity).packageName +
+                        "/" + R.raw.pizza
+            )
+            setMediaController(null)
+            requestFocus()
+        }
+
+        binding.videoView.seekTo(10)
+
+        binding.videoView.setOnCompletionListener {
+            binding.videoView.seekTo(0)
+            binding.videoView.background = Drawable.createFromPath(
+                (activity as AppCompatActivity).packageName +
+                        "/" + R.mipmap.thumbnail)
+        }
+
+        binding.videoView.setOnClickListener {
+            binding.videoView.background = null
+            if (binding.videoView.isPlaying) {
+                binding.videoView.pause()
+                binding.playButton.visibility = View.VISIBLE
+            }
+            else {
+                binding.videoView.start()
+                binding.playButton.visibility = View.GONE
+            }
+        }
 
         return binding.root
     }
